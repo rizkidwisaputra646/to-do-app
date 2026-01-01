ec451cdf97bad6ae127aaed4d40aaa9800f02b62
rizkidwisaputra646 <rizkidwisaputra02344@gmail.com>
2026-01-01 11:00:14 +0700

    commit pertama

---
+++ resources/views/app.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    @vite('resources/css/app.css')
+    <title>Document</title>
+</head>
+<body>
+    <h1 class = "text-3xl text-red-700 bg-amber-200">hai</h1>
+    <div>
+        <div class="bg-amber-300">
+            <p>hai</p>
+        </div>
+    </div>
+    
+</body>
+</html>
